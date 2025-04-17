cf24b60cbf108adbe29ab73a4371ebbaaca71e22
Thomas Matlock <thomasmatlockbba@gmail.com>
2025-04-17 10:10:11 -0500

    add to api

--- backend/StreamBuddy.API/obj/Debug/net9.0/StreamBuddy.API.AssemblyInfo.cs
+++ backend/StreamBuddy.API/obj/Debug/net9.0/StreamBuddy.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreamBuddy.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe9cd8aef6e2171482e1b5c9e44dca62f4f34bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031d5f001477a90b6d7e9e82ec4a5edb6df04dd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreamBuddy.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreamBuddy.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
